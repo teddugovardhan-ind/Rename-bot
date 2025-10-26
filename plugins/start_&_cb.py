1de94cccb90be07b9230c3d54a9eb0fafa4881c9
teddugovardhan-ind <teddugovardhan544@gmail.com>
2025-10-26 19:55:01 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -18,9 +18,9 @@ async def start(client, message):
     await db.add_user(client, message)
     button = InlineKeyboardMarkup([[
         InlineKeyboardButton(
-            '⛅ ᴜᴘᴅᴀᴛᴇs', url='https://t.me/codeflix_bots'),
+            '⛅ ᴜᴘᴅᴀᴛᴇs', url='https://t.me/All_animes_in_teluguu_vs'),
         InlineKeyboardButton(
-            '🌨️ sᴜᴘᴘᴏʀᴛ', url='https://t.me/weebs_support')
+            '🌨️ sᴜᴘᴘᴏʀᴛ', url='https://t.me/All_animes_in_teluguu_vs')
     ], [
         InlineKeyboardButton('❄️ ᴀʙᴏᴜᴛ', callback_data='about'),
         InlineKeyboardButton('❗ ʜᴇʟᴘ', callback_data='help')
@@ -65,9 +65,9 @@ async def cb_handler(client, query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton(
-                    '⛅ Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/codeflix_bots'),
+                    '⛅ Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/All_animes_in_teluguu_vs'),
                 InlineKeyboardButton(
-                    '🌨️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/weebs_support')
+                    '🌨️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/All_animes_in_teluguu_vs')
             ], [
                 InlineKeyboardButton('❄️ ᴀʙᴏᴜᴛ', callback_data='about'),
                 InlineKeyboardButton('❗ ʜᴇʟᴘ', callback_data='help')
